a68b6c6c9148e51b751231c91cf6d2f22110c42c
Paul Koerbitz <paul.koerbitz@theppro.com>
2014-07-05 01:22:39 +0200

    Move parser into Scheme2 and make it very applicativ-y

--- Scheme2.hs
+++ Scheme2.hs
@@ -1,6 +1,14 @@
 module Main where
 
+import           Control.Applicative
+import qualified Data.List as L
 import qualified Data.Map as M
+import           Text.ParserCombinators.Parsec hiding ((<$>), (<|>), many)
+import           System.IO (hFlush, stdout)
+
+----------------------------------------------------------------------
+-- Parsing the Lisp Expressions and writing a REPL
+----------------------------------------------------------------------
 
 data LispVal = Atom String
              | List [LispVal]
@@ -8,67 +16,69 @@ data LispVal = Atom String
              | Boolean Bool
              deriving Show
 
-type Env = M.Map String LispVal
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+/:<=>?@^_~-"
 
-a    = Atom "a"
-b    = Atom "b"
-c    = Atom "c"
-_1   = Number 1
-_2   = Number 2
-_3   = Number 3
-_4   = Number 4
-_5   = Number 5
-plus = Atom "+"
-less = Atom "<"
-_let = Atom "let"
+parseTrue :: Parser LispVal
+parseTrue = string "#t" *> pure (Boolean True)
 
-env :: Env
-env = M.fromList [("a", _3), ("c", _5)]
+parseFalse :: Parser LispVal
+parseFalse = string "#f" *> pure (Boolean False)
 
-lexpr1 = List [plus, _5, _5]
+parseAtom :: Parser LispVal
+parseAtom = Atom <$> ((:) <$> (letter <|> symbol) <*> many (letter <|> digit <|> symbol))
 
-lexpr2 = List [Atom "+", Atom "x", Number 5]
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
 
-lexpr3 = List [Atom "<", List [Atom "+", Number 1, Number 7],
-                         List [Atom "+", Number 1, Number 5], List [Atom undefined]
-                         ]
+parseList :: Parser LispVal
+parseList = string "(" *> (List <$> sepBy parseLispVal spaces) <* string ")"
 
-lexpr4 = List [Atom "list", Atom "a", Atom "b", Atom "c"]
+parseLispVal :: Parser LispVal
+parseLispVal =  parseTrue
+            <|> parseFalse
+            <|> parseAtom
+            <|> parseNumber
+            <|> parseList
 
-lexpr5 = List [Atom "list", lexpr1, Atom "a", Atom "b", Atom "c"]
+parser :: String -> LispVal
+parser s = case parse parseLispVal "lisp" s of
+  Right v -> v
+  Left err -> error $ show err
 
-lexpr6 = List [plus, a, _3]
+eval :: LispVal -> LispVal
+eval (Atom a)                         = Atom a
+eval (Number n)                       = Number n
+eval (Boolean b)                      = Boolean b
+eval (List (Atom a: args))            = case lookup a primitives of
+  Nothing -> error $ "Unknown form: " ++ a
+  Just f -> f $ map eval args
 
-lexpr7 = List [_let, List [List [a,_1], List [b,_2]], List [plus, a, b, c]]
 
--- Task 1: Write a pretty-printer for LispVal which prints Lisp expressions
 primitives :: [(String, [LispVal] -> LispVal)]
 primitives = [("+", foldr (\(Number n) (Number acc) -> Number (acc+n)) (Number 0))
-             ,("-", foldr (\(Number n) (Number acc) -> Number (acc-n)) (Number 0))
-             ,("<", \(arg:args) -> fst $ foldr
-                                   (\(Number n) ((Boolean result), (Number lst)) ->
-                            if result && lst < n
-                            then (Boolean True, Number n)
-                            else (Boolean False, Number lst))
-                   (Boolean True, arg) args)
-             ,("list", List)
+             ,("-", L.foldl1 (\(Number acc) (Number n) -> Number (acc-n)))
+             ,("<", lispCmp "<" (<))
+             ,(">", lispCmp ">" (>))
              ]
 
-eval :: Env -> LispVal -> LispVal
-eval env (Atom a)                         = case M.lookup a env of Just x -> x
-eval env (Number n)                       = Number n
-eval env (Boolean b)                      = Boolean b
-eval env (List (Atom "quote": arg : []))  = arg
-eval env (List [Atom "let", List bindings, body]) =
-  let env' = M.fromList $ map (\(List [Atom x, y]) -> (x, eval env y)) bindings
-      env'' = M.union env' env
-  in eval env'' body
-eval env (List (Atom a: args))            = case lookup a primitives of
-  Nothing -> error $ "Unknown form: " ++ a
-  Just f -> f $ map (eval env) args
+lispCmp :: String -> (Integer -> Integer -> Bool) -> [LispVal] -> LispVal
+lispCmp nm _ []                           = error $ "'" ++ nm ++ "' needs at least one argument"
+lispCmp nm cmp (Number n : [])              = Boolean True
+lispCmp nm cmp (Number n : Number m : rest) = if cmp n m then lispCmp nm cmp (Number m : rest) else Boolean False
 
--- Fehlerbehandlung
 
--- Überprüfe ob die bindings in let die richtige Struktur haben
+pprint :: LispVal -> String
+pprint (Atom a)    = a
+pprint (Number n)  = show n
+pprint (Boolean b) = if b then "#t" else "#f"
+pprint (List l)    = "(" ++ L.intercalate " " (map pprint l) ++ ")"
 
--- Funktionsdefinitionen
+main :: IO ()
+main = do putStr "lisp> "
+          hFlush stdout
+          input <- getLine
+          if input == "quit"
+            then return ()
+            else do putStrLn $ pprint $ eval $ parser input
+                    main
