6373feac9835a0f10011e56a747f6b292a083d10
Paul Koerbitz <paul.koerbitz@theppro.com>
2014-07-05 02:01:43 +0200

    Add in the missing parts from Scheme2 in Scheme3

--- Scheme3.hs
+++ Scheme3.hs
@@ -1,7 +1,10 @@
 module Main where
 
-import qualified Data.Map as M
+import           Control.Applicative
 import qualified Data.List as L
+import qualified Data.Map as M
+import           Text.ParserCombinators.Parsec hiding ((<$>), (<|>), many)
+import           System.IO (hFlush, stdout)
 
 type Env = M.Map String LispVal
 
@@ -37,15 +40,75 @@ eval env (List (Atom a: args))            =
 exec :: Env -> LispVal -> Env
 exec env (List [Atom "setf", (Atom var), expr]) =
   M.insert var (eval env expr) env
-exec env (List [Atom "defn", (List (Atom f : params)), ast]) =
-  M.insert f (Fun (map (\(Atom str) -> str) params) env ast) env
+exec env (List [Atom "defn", (List (Atom f : params)), ast]) = undefined
 
-showProgram = putStrLn . L.intercalate "\n" . map prettyPrint
+showProgram = putStrLn . L.intercalate "\n" . map pprint
 
 initEnv = M.empty
 
 run :: [LispVal] -> Maybe LispVal
 run = M.lookup "main" . L.foldl' exec initEnv
+
+
+----------------------------------------------------------------------
+-- Stuff from Scheme2.hs
+----------------------------------------------------------------------
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+/:<=>?@^_~-"
+
+parseTrue :: Parser LispVal
+parseTrue = string "#t" *> pure (Boolean True)
+
+parseFalse :: Parser LispVal
+parseFalse = string "#f" *> pure (Boolean False)
+
+parseAtom :: Parser LispVal
+parseAtom = Atom <$> ((:) <$> (letter <|> symbol) <*> many (letter <|> digit <|> symbol))
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
+parseList :: Parser LispVal
+parseList = string "(" *> (List <$> sepBy parseLispVal spaces) <* string ")"
+
+parseLispVal :: Parser LispVal
+parseLispVal =  parseTrue
+            <|> parseFalse
+            <|> parseAtom
+            <|> parseNumber
+            <|> parseList
+
+parser :: String -> LispVal
+parser s = case parse parseLispVal "lisp" s of
+  Right v -> v
+  Left err -> error $ show err
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", foldr (\(Number n) (Number acc) -> Number (acc+n)) (Number 0))
+             ,("-", L.foldl1 (\(Number acc) (Number n) -> Number (acc-n)))
+             ,("<", lispCmp "<" (<))
+             ,(">", lispCmp ">" (>))
+             ]
+
+lispCmp :: String -> (Integer -> Integer -> Bool) -> [LispVal] -> LispVal
+lispCmp nm _ []                           = error $ "'" ++ nm ++ "' needs at least one argument"
+lispCmp nm cmp (Number n : [])              = Boolean True
+lispCmp nm cmp (Number n : Number m : rest) = if cmp n m then lispCmp nm cmp (Number m : rest) else Boolean False
+
+pprint :: LispVal -> String
+pprint (Atom a)    = a
+pprint (Number n)  = show n
+pprint (Boolean b) = if b then "#t" else "#f"
+pprint (List l)    = "(" ++ L.intercalate " " (map pprint l) ++ ")"
+
+main :: IO ()
+main = do putStr "lisp> "
+          hFlush stdout
+          input <- getLine
+          if input == "quit"
+            then return ()
+            else do putStrLn $ pprint $ eval M.empty $ parser input
+                    main
 
 
 ----------------------------------------------------------------------
